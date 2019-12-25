f8e4ca2f92cff1382b33f76e40e0f2e28a2c63f0
frontendmonster <frontendmonster@gmail.com>
2019-12-25 18:00:32 +0330

    :sparkles: Add missing CA private keys

--- network/05.start-network.sh
+++ network/05.start-network.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Start orderer and normal peers
 docker-compose -f docker-compose-cli.yaml up -d
+
+# Start CA peers
+export CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org1.example.com/ca && ls *_sk)
+export CA2_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org2.example.com/ca && ls *_sk)
 docker-compose -f docker-compose-ca.yaml up -d
