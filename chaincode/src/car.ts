6807658cd85460963c065f00aa4948edae7580f2
frontendmonster <frontendmonster@gmail.com>
2020-02-11 07:31:55 +0330

    Format code

--- chaincode/src/car.ts
+++ chaincode/src/car.ts
@@ -3,9 +3,9 @@
  */
 
 export class Car {
-    public docType?: string;
-    public color: string;
-    public make: string;
-    public model: string;
-    public owner: string;
+  public docType?: string;
+  public color: string;
+  public make: string;
+  public model: string;
+  public owner: string;
 }
--- chaincode/src/index.ts
+++ chaincode/src/index.ts
@@ -3,6 +3,7 @@
  */
 
 import { FabCar } from './fabcar';
+
 export { FabCar } from './fabcar';
 
-export const contracts: any[] = [ FabCar ];
+export const contracts: any[] = [FabCar];
